74bfcbbd39eae5e63bb2b61a1bc0243238c7a70f
Iliya <korotaevilv@icloud.com>
2023-10-17 20:35:23 +0400

    good morn

--- app/src/main/java/com/bkmz7692/numlog/MainActivity.kt
+++ app/src/main/java/com/bkmz7692/numlog/MainActivity.kt
@@ -7,7 +7,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
+    //good morn
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
