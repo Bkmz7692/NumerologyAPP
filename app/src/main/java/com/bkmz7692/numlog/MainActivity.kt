9f6183adcfde322d9a0f19e46043134ddee578f8
Ilya Korotaev <korotaevilv@icloud.com>
2023-10-18 13:49:47 +0400

    test

--- app/src/main/java/com/bkmz7692/numlog/MainActivity.kt
+++ app/src/main/java/com/bkmz7692/numlog/MainActivity.kt
@@ -7,8 +7,8 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+        //18.10
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var const = 2 //Глобальные переменные
